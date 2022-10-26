bb642df58fa26cfb6d0687d66e3b5ee602cb24d7
matheus1296 <matheuspferreira96@gmail.com>
2022-10-26 18:35:11 -0300

    feat: implementation of the icon button in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from "./components/Title";
+import IconButton from "./components/IconButton";
 
 function App() {
   return (
     <div>
       <Title>Teste</Title>
+        <IconButton onClick={()=>alert("teste")}/>
     </div>
   );
 }
